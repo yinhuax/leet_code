83350ea8a91d7de3a2e851edb78c6df2ee610ee0
yinhuaxi <597290963@qq.com>
2021-07-27 18:46:30 +0800

    feat:
    双向链表

--- datastructure/acwing_study/base_02/LinedListByPointer2.py
+++ datastructure/acwing_study/base_02/LinedListByPointer2.py
@@ -5,3 +5,130 @@
 # @Time    : 2021/7/26 18:32
 # @File    : LinedListByPointer2.py
 
+"""
+实现双向链表
+"""
+
+
+class Node(object):
+
+    def __init__(self, x):
+        self.data = x
+        self.pre = None
+        self.next = None
+
+
+class LinedListByPointer2(object):
+
+    def __init__(self):
+        self.size = 0
+        # 初始化伪头伪尾
+        self.head = Node(0)
+        self.tail = Node(0)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if index < 0 or index >= self.size:
+            return -1
+
+        # 查询优化
+        # 位于左边，从头节点遍历
+        if index + 1 < self.size - index:
+            cur = self.head
+            for _ in range(index + 1):
+                cur = cur.next
+        # 位于右边，从尾节点开始遍历
+        else:
+            cur = self.tail
+            for _ in range(self.size - index):
+                cur = cur.pre
+
+        return cur.val
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        self.size += 1
+        to_add = Node(val)
+        to_add.next = self.head.next
+        to_add.pre = self.head
+        self.head.next.pre = to_add
+        self.head.next = to_add
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        self.size += 1
+        pre = self.tail.pre
+        tail = self.tail
+
+        to_add = Node(val)
+        to_add.pre = pre
+        to_add.next = tail
+        pre.next = to_add
+        tail.pre = to_add
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        # If index is greater than the length,
+        # the node will not be inserted.
+        if index > self.size:
+            return
+
+        # [so weird] If index is negative,
+        # the node will be inserted at the head of the list.
+        if index < 0:
+            index = 0
+
+        # find predecessor and successor of the node to be added
+        if index < self.size - index:
+            pred = self.head
+            for _ in range(index):
+                pred = pred.next
+            succ = pred.next
+        else:
+            succ = self.tail
+            for _ in range(self.size - index):
+                succ = succ.prev
+            pred = succ.prev
+
+        # insertion itself
+        self.size += 1
+        to_add = Node(val)
+        to_add.prev = pred
+        to_add.next = succ
+        pred.next = to_add
+        succ.prev = to_add
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        # if the index is invalid, do nothing
+        if index < 0 or index >= self.size:
+            return
+
+        # find predecessor and successor of the node to be deleted
+        if index < self.size - index:
+            pred = self.head
+            for _ in range(index):
+                pred = pred.next
+            succ = pred.next.next
+        else:
+            succ = self.tail
+            for _ in range(self.size - index - 1):
+                succ = succ.prev
+            pred = succ.prev.prev
+
+        # delete pred.next
+        self.size -= 1
+        pred.next = succ
+        succ.prev = pred
